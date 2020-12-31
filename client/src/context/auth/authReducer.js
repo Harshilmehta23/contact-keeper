841efcc08fc51c37370a73c6f2ad8ea0c9db4e0a
Harshilmehta23 <mharshil60@gmail.com>
2020-12-31 14:30:05 +0530

    Added Function for Login user

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -11,7 +11,15 @@ import {
 
 const auth = (state, action) => {
   switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload,
+      };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
         ...state,
@@ -20,6 +28,8 @@ const auth = (state, action) => {
         loading: false,
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem('token');
       return {
         ...state,
